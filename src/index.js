e9f0bd15c9dc310a8b5fd2b03272d579c3b84561
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-02-03 16:08:16 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import LatestMovieReviewsContainer from './components/LatestMovieReviewsContainer';
 import SearchableMovieReviewsContainer from './components/SearchableMovieReviewsContainer';
 import MovieReviews from '../index'
+
 ReactDOM.render(
   <div className="app">
     <SearchableMovieReviewsContainer />
