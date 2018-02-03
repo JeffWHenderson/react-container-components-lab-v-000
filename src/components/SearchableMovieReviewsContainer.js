d1a6a3849e6035e5e78bb7a6526a116ee4eb1936
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-02-03 15:57:00 +0000

    automatically backed up by learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,5 @@
-// Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react'
+import 'isomorphic-fetch'
+import MovieReviews from './MovieReviews'
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0'
