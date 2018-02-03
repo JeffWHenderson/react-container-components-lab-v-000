38f0425dd8156632b6b4343a7e828ce0ff7316b2
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-02-03 16:03:52 +0000

    automatically backed up by learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-class MovieReviews extents React.Component {
+
+const MovieReviews = ({reviews}) =>  {
+    const reviewList = reviews.map((review, index) => 
+    <div className="review" key={index}> 
+        <h3>{review.display_title}</h3>
+        <h5>{review.headline}</h5>
+        <p>{review.summary_short}</p>
+    </div>
+    )
+
+    return (
+        <div className="review-list">
+            {reviewList}
+        </div>
+    )
+
 
 }
-export default MovieReviews
+
+MovieReviews.defaultProps = {
+    reviews: []
+}
+
+export default MovieReviews;
